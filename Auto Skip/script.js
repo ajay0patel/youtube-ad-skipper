193965c33d99b83069aad5a9705a49951ea68a41
Ajay Patel <iiitianajay@gmail.com>
2022-01-08 01:14:50 +0530

    icon + right ad bar block

--- Auto Skip/script.js
+++ Auto Skip/script.js
@@ -1,11 +1,16 @@
 setInterval(function(){
-	var skip = document.getElementsByClassName("ytp-ad-skip-button");
-	var banner =  document.getElementsByClassName("ytp-ad-overlay-close-button");
-	if(skip != undefined && skip.length > 0){
-		skip[0].click();
-	}
-	if(banner != undefined && banner.length > 0){
-		banner[0].click();
-	}
-	
+		let skip = document.getElementsByClassName("ytp-ad-skip-button");
+		if(skip != undefined && skip.length > 0){
+			skip[0].click();
+		}
+
+		let banner =  document.getElementsByClassName("ytp-ad-overlay-close-button");
+		if(banner != undefined && banner.length > 0){
+			banner[0].click(); // use remove
+		}
+
+		let right_add = document.getElementById("player-ads");
+		if(right_add != undefined){
+			right_add.remove();
+		}
 },3000)
